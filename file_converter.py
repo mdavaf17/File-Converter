32252699cfa22d44ec926dcd1fb809b7f80661ff
XAVAX ​ <mdavaf17@gmail.com>
2023-06-04 15:49:48 +0700

    adding img to pdf feature

--- file_converter.py
+++ file_converter.py
@@ -1,3 +1,4 @@
+import os
 import tkinter
 from tkinter import *
 from tkinter.messagebox import showinfo
@@ -6,7 +7,8 @@ from tkinter.font import Font
 from tkinter import filedialog
 from tkinter.filedialog import askopenfile
 import win32com.client
-import os
+from PIL import Image, ImageTk
+import img2pdf
 
 def raise_frame(frame):
     frame.tkraise()
@@ -79,7 +81,7 @@ pp2pd.grid(row=1, column=2, padx = 20)
 
 # IMAGE TO PDF
 img2pd =Button(converterpage, text="IMAGE TO PDF", width="15", font=lucida, \
-               bg="#ff6e40", fg="white")
+               bg="#ff6e40", fg="white", command=lambda: raise_frame(img2pdfpage))
 img2pd.grid(row=1, column=3, padx = 20)
 
 # PDF TO WORD
@@ -148,6 +150,7 @@ def openPpt():
     powerpoint.Quit()
     showinfo("Complete !", "PPT to PDF Succesfully Converted")
 
+
 # LOGO
 logo4 = PhotoImage(file="logos.png")
 labellogo4 = Label(ppt2pdfpage, image = logo3)
@@ -171,6 +174,48 @@ btn_browse.grid(row=1, column=3)
 btn_back2 = Button(ppt2pdfpage, text="Back", width="5", font="Arial,(12)", \
            command=lambda: raise_frame(converterpage), bg="#1e3d59", fg="white")
 btn_back2.grid(row=3, column=2, pady=30)
+
+# IMAGE TO PDF
+def openImg():
+    fileimg = askopenfile(filetypes=[("Image Files", ".jpg .jpeg .png")])
+    getImgname = fileimg.name
+    pdf = Image.open(getImgname)
+    if pdf.mode == "RGBA" :
+        pdf = pdf.convert("RGB")
+    if ".jpg" in getImgname:
+        newImgPdfname = getImgname.replace(".jpg", ".pdf")
+    if ".jpeg" in getImgname:
+        newImgPdfname = getImgname.replace(".jpeg", ".pdf")
+    if ".png" in getImgname:
+        newImgPdfname = getImgname.replace(".png", ".pdf")
+    pdf.save(newImgPdfname)
+    showinfo("Complete !", "Image to PDF Succesfully Converted")
+
+
+# LOGO
+logo5 = PhotoImage(file="logos.png")
+labellogo5 = Label(img2pdfpage, image = logo3)
+labellogo5.grid(row=0, column=2)
+
+# LABEL CHOOSE FILE
+choose_file = Label(img2pdfpage, text="File", width="15", font=lucida, bg="#1e3d59", \
+                    fg="white")
+choose_file.grid(row=1, column=1, padx = (50,0) , pady = 50)
+
+# TITIK CHOOSE FILE
+titik_choose_file = Label(img2pdfpage, text=":", width="15", font=lucida, bg="#1e3d59", fg="white")
+titik_choose_file.grid(row=1, column=2)
+
+# BROWSE FILE
+btn_browse = Button(img2pdfpage, text="Browse File...", width="15", font=lucida, \
+                    bg="#ffc13b", fg="black", relief=RAISED, command=openImg)
+btn_browse.grid(row=1, column=3)
+
+# BACK FILE CONVERTER
+btn_back3 = Button(img2pdfpage, text="Back", width="5", font="Arial,(12)", \
+           command=lambda: raise_frame(converterpage), bg="#1e3d59", fg="white")
+btn_back3.grid(row=3, column=2, pady=30)
+
 
 frame=Frame(root,relief=RAISED,borderwidth=1)
 frame.pack(fill=X)
